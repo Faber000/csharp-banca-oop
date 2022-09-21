b3fb72481c737fd0932c1ae412f1e7a64ca5dbcb
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-21 12:41:18 +0200

    Aggiungere i file di progetto.

---
+++ Client1.cs
@@ -0,0 +1,15 @@
+﻿public class Client
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Id { get; set; }  
+    public int Salary { get; set; }
+
+    public Client(string name, string lastName, string id, int salary)
+    {
+        this.Name = name;
+        this.LastName = lastName;
+        Id = id;
+        this.Salary = salary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+// list of Loan Objects
+List<Loan> LoanList = new List<Loan>();
+
+// list of Client Objects
+List<Client> ClientList = new List<Client>();
+
+
+
+
+
+
+
+
+
